fb8f62614616d07930283773751f65ae9775cf18
Matheus Daher <matheusdaher@gmail.com>
2025-02-21 21:41:55 -0500

    format " to ' and changing port to 3002

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-import express, { Request, Response } from "express";
-import connectDB from "./config/database";
-import userRoutes from "./routes/userRoutes";
+import express, { Request, Response } from 'express';
+import connectDB from './config/database';
+import userRoutes from './routes/userRoutes';
 
 // Initialize Express app
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Mount user routes
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
 
 // Test route
-app.get("/api/test", (req: Request, res: Response) => {
-  res.json({ message: "Server is running!" });
+app.get('/api/test', (req: Request, res: Response) => {
+  res.json({ message: 'Server is running!' });
 });
 
 // Function to start the server
 const startServer = async () => {
   try {
-    // Connect to MongoDB and start the server
+    // Connect to MongoDB
     await connectDB();
+    // Start the server
     app.listen(port, () => {
       console.log(`Server running at http://localhost:${port}`);
     });
   } catch (error) {
-    console.error("Failed to start server:", error);
+    console.error('Failed to start server:', error);
     process.exit(1);
   }
 };
