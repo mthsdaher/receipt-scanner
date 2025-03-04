160f5e449a0e1ebe1dc08788df8e6a9e71d0887d
Matheus Daher <matheusdaher@gmail.com>
2025-03-03 21:32:47 -0500

    Add Signout route to App component for user logout functionality

--- receipt-scanner-frontend/src/App.tsx
+++ receipt-scanner-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./Signup.tsx";
 import Signin from "./Signin.tsx";
+import Signout from "./pages/Signout/Signout.tsx";
 import Dashboard from "./Dashboard.tsx";
 
 const App: React.FC = () => {
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signout" element={<Signout />} />
       </Routes>
     </Router>
   );
